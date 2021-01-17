af0c89ac8f1e4a0a0a30f8db88d0fa3193af7133
Denis-Iv <sendirian153@gmail.com>
2021-01-17 20:33:07 +0200

    Updated example json

--- Frontend/js/searchFilter.js
+++ Frontend/js/searchFilter.js
@@ -2,17 +2,13 @@ const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
 
 const searchBooks = async searchText => {
-    const res = await fetch('../Frontend/data/books.json');
+    const res = await fetch('../Frontend/data/state_capitals.json');
     const books = await res.json();
 
     let matches = books.filter(book => {
         const regex = new RegExp(`^${searchText}`, 'gi');
-        return book.title.match(regex) || book.author.match(regex);
-    });
-
-    if(searchText.length === 0) {
-        matches = [];
-    }
+        return book.name.match(regex) || book.abbr.match(regex);
+    });    
 
     console.log(matches);
 };
