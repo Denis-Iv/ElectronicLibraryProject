7de70130d53c9928a124bf04c10a6af5c59b0108
Denis-Iv <sendirian153@gmail.com>
2021-01-20 01:15:37 +0200

    Improved search / added sample data

--- Frontend/js/searchFilter.js
+++ Frontend/js/searchFilter.js
@@ -1,27 +1,34 @@
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
+const booksList = document.getElementById('books-list');
 
 search.addEventListener('input', () => searchBooks(search.value));
 
 const searchBooks = async searchText => {
-    const res = await fetch('../Frontend/data/state_capitals.json');
+    const res = await fetch('../Frontend/data/sample.json');
     const books = await res.json();
 
     let matches = books.filter(book => {
         const regex = new RegExp(`^${searchText}`, 'gi');
-        return book.name.match(regex) || book.abbr.match(regex);
+        return book.description.match(regex) || book.imageurl.match(regex);
     });    
 
-    console.log(matches);
-
+    //console.log(matches);
+    
     displayBooks(matches);
 };
 
 const displayBooks = (books) => {
     const htmlString = books.map((book) => {
-        return `<p>book.name</p> `;
+        return `
+        <li class="book">
+            <h2>${book.description}</h2>            
+            <img src="${book.imageurl}"></img>
+        </li> 
+        `;
     }).join('');
-    console.log(htmlString);
+
+    //console.log(htmlString);
 
     booksList.innerHTML = htmlString;
 
