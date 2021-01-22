f6d2241be77cbf0142707dffc3c11dbf0a87ffed
Denis-Iv <sendirian153@gmail.com>
2021-01-22 03:45:03 +0200

    Added more filters and created categories page

--- Frontend/js/fetchFilters.js
+++ Frontend/js/fetchFilters.js
@@ -1,28 +1,69 @@
-const filtersList = document.getElementById('categories-list');
+const categoriesList = document.getElementById('categories-list');
+const authorsList = document.getElementById('authors-list');
+
+const nullCatFilter = `
+                    <li class="filter">
+                        <input class="filter-select" type="radio" name="category" value="none">
+                        <label class="filter-label" for="none">Не филтрирай</label>
+                    </li>    
+                    `;
+
+const nullAuthFilter = `
+                    <li class="filter">
+                        <input class="filter-select" type="radio" name="author" value="none">
+                        <label class="filter-label" for="none">Не филтрирай</label>
+                    </li>    
+                    `;
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetchFilters();
+    fetchCategories();
+    fetchAuthors();
   });
 
-const fetchFilters = async () => {
+const fetchCategories = async () => {
     const res = await fetch('../Frontend/data/sample.json');
-    const filters = await res.json();
+    const categories = await res.json();
 
-    let matches = filters.filter(filter => {       
-        return filter.description;
+    let matches = categories.filter(category => {         
+            return category.description;  
     }); 
 
-    displayFilters(matches);
+    displayCategories(matches);
 };
 
-const displayFilters = (filters) => {
-    const htmlString = filters.map((filter) => {
-        return ` 
-        <li>
-            <p>${filter.description}</p>
-        </li> 
+const displayCategories = (categories) => {
+    const htmlString = categories.map((category) => {       
+        return `         
+        <li class="filter">
+            <input class="filter-select" type="radio" name="category" value="${category.description}">
+            <label class="filter-label" for="${category.description}">${category.description}</label>
+        </li>    
         `;
-    }).join();
+    }).join('');
 
-    filtersList.innerHTML = htmlString;
+    categoriesList.innerHTML = nullCatFilter + htmlString;
+}
+
+const fetchAuthors = async () => {
+    const res = await fetch('../Frontend/data/sample.json');
+    const authors = await res.json();
+
+    let matches = authors.filter(author => {         
+            return author.description;  
+    }); 
+
+    displayAuthors(matches);
+};
+
+const displayAuthors = (authors) => {
+    const htmlString = authors.map((author) => {       
+        return `         
+        <li class="filter">
+            <input class="filter-select" type="radio" name="author" value="${author.description}">
+            <label class="filter-label" for="${author.description}">${author.description}</label>
+        </li>    
+        `;
+    }).join('');
+    
+    authorsList.innerHTML = nullAuthFilter + htmlString;
 }
