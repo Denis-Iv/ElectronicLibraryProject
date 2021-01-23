253ace4135155def68f7e1eeecab1bafb4664ce4
Denis-Iv <sendirian153@gmail.com>
2021-01-23 15:25:10 +0200

    Cleaned up code and added styling to "show more" button

--- Frontend/js/loadComponents.js
+++ Frontend/js/loadComponents.js
@@ -5,3 +5,21 @@ $.get("components/navigation.html", function(data){
 $.get("components/footer.html", function(data){
     $("#footer-placeholder").replaceWith(data);
 });
+
+$(function() {        
+    $('.filters-list').each(function() {
+        var $list = $(this);
+        $list.before('<span class="more-less">»Покажи по-малко«</span>');
+        $list.find('.filter:gt(5)').hide();        
+    });    
+  
+    $('.more-less').click(function() {
+        var $btn = $(this);
+        $btn.next().find('.filter:gt(5)').toggle();    
+        $btn.text($btn.text() == '»Покажи по-малко«' ? '«Покажи повече»' : '»Покажи по-малко«');   
+    });
+
+    setTimeout(function() {
+            $('.more-less').trigger('click');
+        },40);   
+})
